050b8bae143826fdbd5ff0e798bfc82ec552e999
BagusFary <bagusfaryananta@gmail.com>
2023-11-04 20:32:57 +0700

    mari bang

---
+++ app/products/page.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Product() {
+  return (
+    <div className="flex justify-center mt-10">
+      <h1 className="text-2xl font-bold">Halaman Products</h1>
+    </div>
+  );
+}
---
+++ app/users/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Users() {
+  return (
+    <div className='flex justify-center mt-10'>
+        <h1 className='text-2xl font-bold'>Halaman Users</h1>
+    </div>
+  )
+}
